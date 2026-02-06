b8ff77e7f0cf9e42418c0803f1e72056f0da2d4a
Harsh Yadav <hy5451819@gmail.com>
2026-02-06 21:54:34 +0530

    DAY 8

--- 100 Days Code/Day7.java
+++ 100 Days Code/Day7.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Day7 {
     public static void main(String[] args) {
-    Scanner sc=new Scanner (System.in);
+    Scanner scanner=new Scanner (System.in);
     System.out.print("Enter the number:");
-    int num=sc.nextInt();
-System.out.println("Sqaure of "+num+" is "+num*num);
+    int number=scanner.nextInt();
+System.out.println("Sqaure of "+number+" is "+number*number);
     }
 }
