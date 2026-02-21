21ab725fb9ad6378dc585c0f7a2dbc78b943d3d1
Harsh Yadav <hy5451819@gmail.com>
2026-02-21 23:31:24 +0530

    Basic q4

---
+++ Logic buiding Quiz GOG/questionB4.java
@@ -0,0 +1,10 @@
+public class questionB4 {
+    public static void main(String[] args) {
+        int n = 3;
+        int squareSum = 0;
+        for(int i = 1; i <= n; i++){
+            squareSum = squareSum + (i*i);
+        }
+        System.out.println(squareSum);
+    }
+}
