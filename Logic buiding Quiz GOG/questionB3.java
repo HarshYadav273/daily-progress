d7a6c21064db896063230d932c09b074976b566d
Harsh Yadav <hy5451819@gmail.com>
2026-02-21 23:21:23 +0530

    3rd question basic

---
+++ Logic buiding Quiz GOG/questionB3.java
@@ -0,0 +1,11 @@
+public class questionB3{
+public static void main(String[] args) {
+    int n = 5;
+    int sum =0;
+    
+    for(int i = 1; i <= n; i++){
+        sum = sum +1 ;
+    }
+    System.out.println(sum);
+}
+}
